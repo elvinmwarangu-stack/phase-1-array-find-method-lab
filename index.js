f2c74aeb600a8930fa0d38deb79dd0bb767271b9
elvinmwarangu-stack <elvin.mwarangu@student.moringaschool.com>
2025-09-15 07:38:33 +0300

    commit files

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+// superbowl.js
+
+function superbowlWin(record) {
+  const win = record.find(entry => entry.result === "W");
+  return win ? win.year : undefined;
+}
+
+module.exports = superbowlWin;
